e9fe59b24eef7b19336253cd931aac6d329bdf3b
PARK-CHAN-SOL <cksthf7374@gmail.com>
2024-07-03 12:09:42 +0900

    feat: Add UTF-8 filter (modify web.xml)

---
+++ src/main/java/com/sol/app/utilgit/DBConnection.java
@@ -0,0 +1,22 @@
+package com.sol.app.utilgit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	
+	public Connection getConnection() throws Exception {
+		String username = "";
+		String password = "";
+		
+		String url  = "jdbc:oracle:thin:@ip:port:sid";
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		Class.forName(driver);
+		
+		Connection con = DriverManager.getConnection(url, username, password);
+		
+		return con;
+	}
+}
